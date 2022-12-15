b9f0d648cfb8d5f4347f8c38b0c6ebce848535d5
rytuks1337 <112757266+rytuks1337@users.noreply.github.com>
2022-12-15 15:28:40 +0200

    Added Tests

--- app/src/test/java/com/example/charcounter/ExampleUnitTest.java
+++ app/src/test/java/com/example/charcounter/ExampleUnitTest.java
@@ -11,7 +11,27 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void calculateWordsWithSpace() {
+        final String starting_text="Hello World I am creator";
+        final String expected_result= "5";
+        assertEquals(expected_result,calculations.calcWords(starting_text));
+    }
+    @Test
+    public void calculateOneWord() {
+        final String starting_text="Megatastic";
+        final String expected_result= "1";
+        assertEquals(expected_result,calculations.calcWords(starting_text));
+    }
+    @Test
+    public void calculateWordsWithNumbers() {
+        final String starting_text="Player FalcCon45 has entered the Game";
+        final String expected_result= "6";
+        assertEquals(expected_result,calculations.calcWords(starting_text));
+    }
+    @Test
+    public void calculateCharsWithSpacesAndSpecialCharacters() {
+        final String starting_text="I am the one who knocks!";
+        final String expected_result= "24";
+        assertEquals(expected_result,calculations.calcChars(starting_text));
     }
 }
